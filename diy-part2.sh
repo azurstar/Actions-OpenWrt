b182deeddc7203bee6c94cda9887bcba050534b3
azurstar <91844313+azurstar@users.noreply.github.com>
2025-01-22 17:29:06 +0800

    update

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,4 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' feeds/luci/modules/luci-mod-system/htdocs/luci-static/resources/view/system/flash.js
